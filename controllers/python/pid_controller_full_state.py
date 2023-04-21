0629e25c43711e117d227a32ae9825f9d6252975
knmcguire <kimberly@bitcraze.io>
2023-04-21 11:28:24 +0200

    got controller kind of workish

--- controllers/python/pid_controller_full_state.py
+++ controllers/python/pid_controller_full_state.py
@@ -23,52 +23,70 @@ class PIDController3D:
 
     def update(self, actual_value, desired_value, dt):
         return [self.x.update(actual_value[0], desired_value[0], dt),
-                self.y.update(actual_value[0], desired_value[1], dt),
-                self.z.update(actual_value[0], desired_value[2], dt)]
+                self.y.update(actual_value[1], desired_value[1], dt),
+                self.z.update(actual_value[2], desired_value[2], dt)]
 
 class PIDControllerFullState:
 
-    def __init__(self, gainsPos, gainsVel, gainsAtt):
-        self.pos = PIDController3D(gainsPos[0], gainsPos[1], gainsPos[2])
-        self.vel = PIDController3D(gainsVel[0], gainsVel[1], gainsVel[2])
-        self.att = PIDController3D(gainsAtt[0], gainsAtt[1], gainsAtt[2])
+    def __init__(self, gains_pos, gains_vel, gains_att, gains_att_rate):
+        self.pos = PIDController3D(gains_pos[0], gains_pos[1], gains_pos[2])
+        self.vel = PIDController3D(gains_vel[0], gains_vel[1], gains_vel[2])
+        self.att = PIDController3D(gains_att[0], gains_att[1], gains_att[2])
+        self.rate = PIDController3D(gains_att_rate[0], gains_att_rate[1], gains_att_rate[2])
 
 
-    def update(self, actual_value, desired_value, pos_mode, desired_thrust, dt):
+    def update(self, actual_value, desired_value, pos_mode, att_mode, desired_thrust, dt):
+
+        # pos_mode = 0: position control
+        # pos_mode = 1: velocity control
+        # pos_mode = 2: attitude control
+
+        # att_mode = 0: attitude control
+        # att_mode = 1: attitude rate control
 
         # First run position controller
-        action_pos = self.pos.update(actual_value, desired_value, dt)
+        action_pos = self.pos.update(actual_value[0], desired_value[0], dt)
 
         # setup setpoints for velocity controller
         setpoint_vel = [0, 0, 0]
         for i in range(3):
             if pos_mode[i] == 0:
-               setpoint_vel[i] = action_pos[i]
+                setpoint_vel[i] = action_pos[i]
             elif pos_mode[i] == 1:
-                setpoint_vel[i] = desired_value[i]
+                setpoint_vel[i] = desired_value[1][i]
 
         # Run velocity controller
-        action_vel = self.vel.update(actual_value, desired_value, dt)
+        action_vel = self.vel.update(actual_value[1], setpoint_vel, dt)
 
         # setup setpoints for attitude controller
         setpoint_att = [0, 0, 0]
         for i in range(2):
-            if pos_mode[i] == 0 or pos_mode[i]==1:
+            if pos_mode[i] == 0 or pos_mode[i] == 1:
                 setpoint_att[i] = action_vel[i]
             elif pos_mode[i] == 2:
-                setpoint_att[i] = desired_value[i]
+                setpoint_att[i] = desired_value[2][i]
+
+        setpoint_att[2] = desired_value[2][2]
 
         if pos_mode[2]== 0 or pos_mode[2]==1:
             action_thrust = action_vel[2]
         elif pos_mode[2] == 2:
             action_thrust = desired_thrust
 
-        action_att = [0, 0, 0]
+        action_att = self.att.update(actual_value[2], setpoint_att, dt)
 
+        # setup setpoints for attitude rate controller
+        setpoint_rate = [0, 0, 0]
+        for i in range(3):
+            if att_mode[i] == 0:
+                setpoint_rate[i] = action_att[i]
+            elif att_mode[i] == 1:
+                setpoint_rate[i] = desired_value[3][i]
 
+        # Run attitude rate controller
+        action_rate = self.rate.update(actual_value[3], setpoint_rate, dt)
 
-        # Run attitude controller
-        action_att = self.att.update(actual_value, desired_value, dt)
+        return action_rate[0], action_rate[1], action_rate[2], action_thrust
 
 
 
---
+++ webots/controllers/crazyflie_controller_py2/crazyflie_controller_py2.py
@@ -0,0 +1,239 @@
+#  ...........       ____  _ __
+#  |  ,-^-,  |      / __ )(_) /_______________ _____  ___
+#  | (  O  ) |     / __  / / __/ ___/ ___/ __ `/_  / / _ \
+#  | / ,..´  |    / /_/ / / /_/ /__/ /  / /_/ / / /_/  __/
+#     +.......   /_____/_/\__/\___/_/   \__,_/ /___/\___/
+
+# MIT License
+
+# Copyright (c) 2022 Bitcraze
+
+# @file crazyflie_controllers_py.py
+# Controls the crazyflie motors in webots in Python
+
+"""crazyflie_controller_py controller."""
+
+
+from controller import Robot
+from controller import Motor
+from controller import InertialUnit
+from controller import GPS
+from controller import Gyro
+from controller import Keyboard
+from controller import Camera
+from controller import DistanceSensor
+
+from math import cos, sin
+
+import sys
+sys.path.append('../../../controllers/')
+
+from python.pid_controller_full_state import PIDControllerFullState
+
+
+robot = Robot()
+
+timestep = int(robot.getBasicTimeStep())
+
+## Initialize motors
+m1_motor = robot.getDevice("m1_motor");
+m1_motor.setPosition(float('inf'))
+m1_motor.setVelocity(-1)
+m2_motor = robot.getDevice("m2_motor");
+m2_motor.setPosition(float('inf'))
+m2_motor.setVelocity(1)
+m3_motor = robot.getDevice("m3_motor");
+m3_motor.setPosition(float('inf'))
+m3_motor.setVelocity(-1)
+m4_motor = robot.getDevice("m4_motor");
+m4_motor.setPosition(float('inf'))
+m4_motor.setVelocity(1)
+
+## Initialize Sensors
+imu = robot.getDevice("inertial unit")
+imu.enable(timestep)
+gps = robot.getDevice("gps")
+gps.enable(timestep)
+gyro = robot.getDevice("gyro")
+gyro.enable(timestep)
+camera = robot.getDevice("camera")
+camera.enable(timestep)
+range_front = robot.getDevice("range_front")
+range_front.enable(timestep)
+range_left = robot.getDevice("range_left")
+range_left.enable(timestep)
+range_back = robot.getDevice("range_back")
+range_back.enable(timestep)
+range_right = robot.getDevice("range_right")
+range_right.enable(timestep)
+
+## Get keyboard
+keyboard = Keyboard()
+keyboard.enable(timestep)
+
+## Initialize variables
+actual_state = [[0 for x in range(3)] for y in range(4)]
+desired_state = [[0 for x in range(3)] for y in range(4)]
+pos_mode = [0, 0, 0]
+att_mode = [0, 0, 0]
+
+past_time = robot.getTime()
+past_x_global = 0
+past_y_global = 0
+
+## Initialize PID gains.
+gains_pid_pos = [[0 for x in range(3)] for y in range(3)]
+gains_pid_pos[0][0] = 1.0
+gains_pid_pos[0][1] = 0.0
+gains_pid_pos[0][2] = 0.0
+gains_pid_pos[1][0] = 1.0
+gains_pid_pos[1][1] = 0.0
+gains_pid_pos[1][2] = 0.0
+gains_pid_pos[2][0] = 15
+gains_pid_pos[2][1] = 0
+gains_pid_pos[2][2] = 0
+
+gains_pid_vel = [[0 for x in range(3)] for y in range(3)]
+gains_pid_vel[0][0] = 1.0
+gains_pid_vel[0][1] = 0.0
+gains_pid_vel[0][2] = 0.0
+gains_pid_vel[1][0] = 1.0
+gains_pid_vel[1][1] = 0.0
+gains_pid_vel[1][2] = 0.0
+gains_pid_vel[2][0] = 1.0
+gains_pid_vel[2][1] = 0.0
+gains_pid_vel[2][2] = 0.0
+
+gains_pid_att = [[0 for x in range(3)] for y in range(3)]
+gains_pid_att[0][0] = 1.0
+gains_pid_att[0][1] = 0.0
+gains_pid_att[0][2] = 0.0
+gains_pid_att[1][0] = 1.0
+gains_pid_att[1][1] = 0.0
+gains_pid_att[1][2] = 0.0
+gains_pid_att[2][0] = 1.0
+gains_pid_att[2][1] = 0.0
+gains_pid_att[2][2] = 0.0
+
+gains_pid_rate = [[0 for x in range(3)] for y in range(3)]
+gains_pid_rate[0][0] = 1.0
+gains_pid_rate[0][1] = 0.0
+gains_pid_rate[0][2] = 0.0
+gains_pid_rate[1][0] = 1.0
+gains_pid_rate[1][1] = 0.0
+gains_pid_rate[1][2] = 0.0
+gains_pid_rate[2][0] = 1.0
+gains_pid_rate[2][1] = 0.0
+gains_pid_rate[2][2] = 0.0
+
+controller = PIDControllerFullState(gains_pid_pos, gains_pid_vel, gains_pid_att, gains_pid_rate)
+
+# Main loop:
+while robot.step(timestep) != -1:
+
+    dt = robot.getTime() - past_time;
+
+    ## Get measurements
+    roll = imu.getRollPitchYaw()[0]
+    pitch = imu.getRollPitchYaw()[1]
+    yaw = imu.getRollPitchYaw()[2]
+    roll_rate = gyro.getValues()[0]
+    pitch_rate = gyro.getValues()[1]
+    yaw_rate = gyro.getValues()[2]
+    altitude = gps.getValues()[2]
+    x_global = gps.getValues()[0]
+    vx_global = (x_global - past_x_global)/dt
+    y_global = gps.getValues()[1]
+    vy_global = (y_global - past_y_global)/dt
+
+    cosyaw = cos(yaw)
+    sinyaw = sin(yaw)
+    x_body = x_global * cosyaw + y_global * sinyaw
+    y_body = - x_global * sinyaw + y_global * cosyaw
+    z_body = altitude
+    vx_body = vx_global * cosyaw + vy_global * sinyaw
+    vy_body = - vx_global * sinyaw + vy_global * cosyaw
+    vz_body = altitude
+
+    actual_state[0][0] = x_body
+    actual_state[0][1] = y_body
+    actual_state[0][2] = z_body
+    actual_state[1][0] = vx_body
+    actual_state[1][1] = vy_body
+    actual_state[1][2] = vz_body
+    actual_state[2][0] = roll
+    actual_state[2][1] = pitch
+    actual_state[2][2] = yaw
+    actual_state[3][0] = roll_rate
+    actual_state[3][1] = pitch_rate
+    actual_state[3][2] = yaw_rate
+
+    desired_state[0][0] = 0
+    desired_state[0][1] = 0
+    desired_state[0][2] = 1
+    desired_state[1][0] = 0
+    desired_state[1][1] = 0
+    desired_state[1][2] = 0
+    desired_state[2][0] = 0
+    desired_state[2][1] = 0
+    desired_state[2][2] = 0
+    desired_state[3][0] = 0
+    desired_state[3][1] = 0
+    desired_state[3][2] = 0
+
+    forwardDesired = 0
+    sidewaysDesired = 0
+    yawDesired = 0
+
+    key = keyboard.getKey()
+    while key>0:
+        if key == Keyboard.UP:
+            forwardDesired += 0.2
+        elif key == Keyboard.DOWN:
+            forwardDesired -= 0.2
+        elif key == Keyboard.RIGHT:
+            sidewaysDesired -= 0.2
+        elif key == Keyboard.LEFT:
+            sidewaysDesired += 0.2
+        elif key == ord('Q'):
+            yawDesired =  + 0.5
+        elif key == ord('E'):
+            yawDesired = - 0.5
+
+        key = keyboard.getKey()
+
+    ## Example how to get sensor data
+    ## range_front_value = range_front.getValue();
+    ## cameraData = camera.getImage()
+
+    desired_state[0][0] = forwardDesired
+    desired_state[0][1] = sidewaysDesired
+    desired_state[0][2] = 1
+    desired_state[3][0] = yawDesired
+
+    pos_mode[0] = 1
+    pos_mode[1] = 1
+    pos_mode[2] = 0
+
+    att_mode[0] = 0
+    att_mode[1] = 0
+    att_mode[2] = 1
+
+    cmd_roll, cmd_pitch, cmd_yaw, cmd_thrust = controller.update(actual_state, desired_state, pos_mode, att_mode, 0, dt)
+
+    ## Motor mixing
+    motor_power_m1 =  cmd_thrust - cmd_roll + cmd_pitch + cmd_yaw
+    motor_power_m2 =  cmd_thrust - cmd_roll - cmd_pitch - cmd_yaw
+    motor_power_m3 =  cmd_thrust + cmd_roll - cmd_pitch + cmd_yaw
+    motor_power_m4 =  cmd_thrust + cmd_roll + cmd_pitch - cmd_yaw
+
+    m1_motor.setVelocity(-motor_power_m1)
+    m2_motor.setVelocity(motor_power_m2)
+    m3_motor.setVelocity(-motor_power_m3)
+    m4_motor.setVelocity(motor_power_m4)
+
+    past_time = robot.getTime()
+    past_x_global = x_global
+    past_y_global = y_global
+
+    pass
